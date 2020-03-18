bbcfb4847f027a9ad796bf09cf6d140803fe2040
Hung Nguyen Huy <hungnh1812@gmail.com>
2020-03-18 14:12:39 +0700

    - Commit all before afternoon Mar 18: 	RayCast 	LayerMask 	Step 7-8 tutorial

---
+++ RollABall/Assets/Scripts/SpawnController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public GameObject collector;
+    public GameObject spawnObject;
+    public LayerMask layerMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0)){
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100, layerMask)) {
+                GameObject game = Instantiate(spawnObject, hit.point, Quaternion.identity);
+                game.transform.parent = collector.transform;
+                game.SetActive(true);
+            }
+        }
+    }
+}
