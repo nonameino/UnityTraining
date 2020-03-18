3a12322e39a04ff62e5b5707b254bef934e461c2
Hung Nguyen Huy <hungnh1812@gmail.com>
2020-03-18 14:49:52 +0700

    + Add feature: Player move to the first target object in list

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -1,22 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public Text countText;               
 
     private Rigidbody rb;
+    private int count;
+    public LayerMask layerMask;
+    private List<Vector3> targetStack;
 
     public void Start() {
         rb = this.GetComponent<Rigidbody>();
+        count = 0;
+        countText.text = "Count: 0";
+        targetStack = new List<Vector3>();
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonUp(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray, out raycastHit, 100, layerMask)) {
+
+                //Add list target
+                targetStack.Add(raycastHit.point);
+            }
+        }
     }
 
     public void FixedUpdate() {
-        float moveVertical = Input.GetAxis("Vertical");
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        // float moveVertical = Input.GetAxis("Vertical");
+        // float moveHorizontal = Input.GetAxis("Horizontal");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed;
-        rb.AddForce(movement);
+        // Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed;
+        // rb.AddForce(movement);
+
+        if (targetStack.Count > 0) {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, targetStack[0], speed*Time.deltaTime);
+        }
+    }
+
+    protected void OnTriggerEnter(Collider other) {
+        
+        if (other.gameObject.CompareTag("PickUp")) {
+            //Disactive collider object
+            other.transform.parent.gameObject.SetActive(false);
+
+            //Update count
+            count = count + 1;
+            SetCountText(count);
+            
+            //Remove collider position
+            targetStack.RemoveAt(GetIndexOfCollider(other));
+        }
+    }
+
+    private int GetIndexOfCollider(Collider collider) {
+        return targetStack.IndexOf(GetParentPositionOfCollider(collider));
+    }
+
+    private Vector3 GetParentPositionOfCollider(Collider collider) {
+        return collider.transform.parent.transform.position;
+    }
+
+    private void SetCountText(int count) {
+        countText.text = "Count: " + count.ToString();
     }
 }
