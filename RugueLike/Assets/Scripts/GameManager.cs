582b0cc89f9a326276ae16ae3e7d7b0ca98e311a
Hung Nguyen Huy <hungnh1812@gmail.com>
2020-03-20 16:33:27 +0700

    - Step 5: Game Manager

---
+++ RugueLike/Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    private BoardManager boardScript;
+	private int level = 1;
+
+    //Awake is always called before any Start functions
+	void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+		
+		DontDestroyOnLoad(gameObject);
+        
+		boardScript = GetComponent<BoardManager>();
+		InitGame();
+	}
+
+    void InitGame() {
+		boardScript.SetupScene(level);
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
